963e91ec7289b9a25cf127996b2255dc537dd281
424-ali-alfadhli <66500381+424-ali-alfadhli@users.noreply.github.com>
2020-06-29 15:35:00 +0300

    .

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -1,29 +1,40 @@
 // السؤال الأول
 // 1
-
+let name = ("علي")
 // 2
-
+let age = ("15")
 // 3 and 4
-
+console.log("انا" + "" + name +  "  " + وعمري +" " + age )
 // 5 and 6
-
+age *= 5
+console.log(age)
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // السؤال الثاني
 // 1
-
+let hobby = ['Football,running'];
 // 2
-
+hobby.push(14);
 // 3
-
+hobby.pop();
+console.log(hobby);
 // 4
+let web ={
+languagone = html
+languagtwo = css
+languagthree = javascript
 
+
+}
 // 5
-
+let ta =["Hussain,Rahaf,Moudhi,Nancy"];
 // 6
+let choice = {
+name = web,
 
+}
 // 7
-
+console.log(ta = 0);
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // البونص
